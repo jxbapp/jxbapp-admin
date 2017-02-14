ade469c417745edbdd75fdc64e4f4ad4cdcdacd1
朱松杰 <vurtne0511@gmail.com>
2017-02-14 16:56:14 +0800

    初始化

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// gulp 基础包 和 gulp常用插件包
+const gulp = require('gulp');
+
+const less = require('gulp-less');
+
+const uglify = require('gulp-uglify');
+
+const rename = require('gulp-rename');
+
+const path = require('path');
