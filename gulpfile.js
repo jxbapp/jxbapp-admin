ce02fdaad75f97e7c1651ad391dea00ffe499fe4
朱松杰 <vurtne0511@gmail.com>
2017-02-15 15:28:42 +0800

    0.0.3 alpha

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,40 @@ const gulp = require('gulp');
 
 const less = require('gulp-less');
 
+const cleanCss = require('gulp-clean-css');
+
 const uglify = require('gulp-uglify');
 
 const rename = require('gulp-rename');
 
 const path = require('path');
+
+// 用于错误通知
+const notify = require("gulp-notify");
+
+// 错误处理
+let handlerError = function () {
+
+    var args = Array.prototype.slice.call(arguments);
+
+    notify.onError({
+        title: 'compile error',
+        message: '<%=error.message %>'
+    }).apply(this, args);
+
+    //替换为当前对象
+    this.emit(); //提交
+};
+
+var src = path.join('./', 'src');
+var dist = path.join('./', 'dist');
+
+let file = 'less/jxbapp-admin.less';
+
+gulp.task('less',  () =>
+     gulp.src(file)
+        .pipe(less())
+        .on('error',handlerError)
+        .pipe(cleanCss({ keepSpecialComments: 1 }))
+        .pipe(gulp.dest(dist))
+);
